4c144f2a290d32cb94d74a1fb7c5425af83e7edc
Chris Duxbury <dux.chris@gmail.com>
2021-09-23 13:34:17 -0400

    chore(client): rename app component

--- client/src/components/app/index.ts
+++ client/src/components/app/index.ts
@@ -1,11 +1,11 @@
 import { AppComponent } from './app.component';
-import { ComponentDefinition} from '../../common';
+import { ComponentDefinition } from '../../common';
 export * from './app.routes';
 
 const appTemplate = require('./app.template.html');
 
-export const AppComponentDefiniton:ComponentDefinition = {
-    name: 'map-large-app',
+export const AppComponentDefiniton: ComponentDefinition = {
+    name: 'dir-browser-app',
     definition: {
         viewModel: AppComponent,
         template: appTemplate
